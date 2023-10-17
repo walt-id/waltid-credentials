ca2ab47bad45ac5501d075506fdcb4bfb4340f35
waltkb <68587968+waltkb@users.noreply.github.com>
2023-10-17 17:18:25 +0200

    Update baseURL path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   srcDir: "src",
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+
+  app: {
+    baseURL: "/vc-repository/"
+  }
 })
