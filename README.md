e130f494f1a08691ae9cc5b99ccf4f7c12cbdd23
waltkb <68587968+waltkb@users.noreply.github.com>
2023-10-17 17:09:04 +0200

    Added nuxt content

---
+++ README.md
@@ -0,0 +1,1 @@
+# walt.id Verifiable Credentials Repository
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  srcDir: "src",
+  devtools: { enabled: true },
+  modules: ['@nuxt/content']
+})
