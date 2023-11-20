606937ddec7b153bbe7c4c03c589cded9dbdb973
waltkb <68587968+waltkb@users.noreply.github.com>
2023-11-20 15:15:45 +0100

    revert nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: "src",
-  devtools: { enabled: true },
-  modules: [
-    "@nuxt/content",
-    ["@unocss/nuxt", { autoImport: false }],
-    "nuxt-icon",
-    "nuxt-monaco-editor",
-    "@nuxt/image",
-  ],
-  content: {
-    highlight: {
-      theme: "github-dark",
-      preload: ["json", "kotlin", "http", "js", "ts", "md", "shell"],
+    srcDir: "src",
+
+    devtools: { enabled: true },
+
+    modules: [
+        "@nuxt/content",
+        ["@unocss/nuxt", { autoImport: false }],
+        "nuxt-icon",
+        "nuxt-monaco-editor",
+        "@nuxt/image",
+        "nuxt-security"
+    ],
+
+    security: {
+        corsHandler: {
+            origin: "*",
+            methods: "*"
+        },
+        rateLimiter: false
     },
-  },
+
+
+    content: {
+        highlight: {
+            theme: "github-dark",
+            preload: ["json", "kotlin", "http", "js", "ts", "md", "shell"]
+        }
+    }
 });
